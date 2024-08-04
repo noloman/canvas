b1d81b6c305dd08acb7c57f9793cf2a5a7c38d5f
Manuel Lorenzo <manulorenzop@gmail.com>
2024-08-04 18:40:43 +0200

    views

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 )
 
 require (
+	github.com/maragudk/gomponents v0.20.4
+	github.com/maragudk/gomponents-heroicons v0.5.0
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/sync v0.7.0
 )
--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,5 @@
 package server_test
 
-
 import (
 	"net/http"
 	"testing"
@@ -21,6 +20,6 @@ func TestServer_Start(t *testing.T) {
 
 		resp, err := http.Get("http://localhost:8081/")
 		is.NoErr(err)
-		is.Equal(http.StatusNotFound, resp.StatusCode)
+		is.Equal(http.StatusOK, resp.StatusCode)
 	})
 }
